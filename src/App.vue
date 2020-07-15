02eb91c3ef70626aed55a8ff0d3c1f87f58f4f7d
pabloyanezb <pabloyanezbarriga@gmail.com>
2020-07-15 17:54:38 -0400

    [TAREA] contador-vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,61 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="background">
+    <div class="container">
+      <h1>CONTADOR DE CLICKS</h1>
+      <Contador/>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Contador from './components/Contador.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Contador
   }
 }
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Knewave&display=swap');
+body {
+  margin: 0 !important;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+.background {
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  overflow: auto;
+  background: linear-gradient(to bottom, #374785, #24305e) fixed;
+}
+.container {
+  background-color: #24305e;
+  border-radius: 1px;
+  padding: 25px;
+  margin: auto;
+  backdrop-filter: blur(5px);
+  max-width: 340px;
+  height: fit-content;
+  position: absolute;
+  top: 0; left: 0; bottom: 25%; right: 0;
+  color: #F8E9A1;
+  box-shadow: -8px 8px 0px -4px rgba(0, 0, 0, 0.623);
+}
+h1 {
+  color: #ff6161;
+  text-shadow: 1pt 1pt #d2e4eef1;
+  font-weight: 500;
+  font-size: 50px;
+  margin: 0;
+  font-family: 'Knewave', cursive;
 }
 </style>
---
+++ src/components/Contador.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <p v-if="!hovered" class="no-visible">Este texto es invisible</p>
+    <p v-if="contador!=1 && hovered">Has clickeado {{contador}} veces</p>
+    <p v-if="contador==1 && hovered">Has clickeado {{contador}} vez</p>
+    <button @click="clic()" @mouseover="hover()">click!</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Contador",
+  data() {
+    return {
+      contador: 0,
+      hovered: false
+    };
+  },
+  methods: {
+    clic() {
+      this.contador++;
+      localStorage.contador = this.contador;
+    },
+    hover() {
+      console.log("mouseover");
+      this.hovered = true;
+    }
+  },
+  mounted() {
+    if (localStorage.contador) {
+      this.contador = localStorage.contador;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.no-visible {
+  color: transparent;
+}
+button {
+  padding: 10px 35px;
+  background-color: #f8e9a1;
+  color: #242d52;
+  border: 0;
+  border-radius: 5px;
+  font-size: 20px;
+  font-weight: 900;
+  font-family: "Knewave", cursive;
+}
+button:hover {
+  box-shadow: 0 0 3pt 0pt #fef9c7;
+}
+button:focus {
+  outline: none;
+}
+</style>
